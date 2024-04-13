b6559b193e573a1322d4a0a33b471aad20970bd2
RussianGravy <valentinejones1215@gmail.com>
2024-04-13 14:01:53 -0700

    removed some stuff, added new intro

--- js/script.js
+++ js/script.js
@@ -10,7 +10,19 @@ circle_spacing = wdow.width/20;
 var rows = [];
 var cols =[];
 
+for (var y = (circle_radius+circle_spacing)/2; y < wdow.height; y += circle_radius+circle_spacing) {
+    rows.push(y);
+    for (var x = (circle_radius+circle_spacing)/2; x < wdow.width-circle_spacing/2; x += circle_radius+circle_spacing) {
+        drawCircle(x, y)
+        cols.push(x);
+    }
+}
+
 function drawCircle(x, y, fill = false, fillColor = 'grey', stroke = 'grey'){
+    var temp1 = ctx.strokeStyle;
+    var temp2 = ctx.fillStyle;
+    var temp3 = ctx.lineWidth;
+
     ctx.strokeStyle = stroke;
     ctx.fillStyle = fillColor;
     ctx.lineWidth = 1;
@@ -23,11 +35,18 @@ function drawCircle(x, y, fill = false, fillColor = 'grey', stroke = 'grey'){
 
     ctx.stroke();
     
-    if(fill)
-        ctx.fill();
+    if(fill){
+        ctx.fill();}
+
+    ctx.strokeStyle = temp1;
+    ctx.fillStyle = temp2;
+    ctx.lineWidth = temp3;
 }
 
 function drawEdge(x1, y1, x2, y2){
+    var temp1 = ctx.strokeStyle;
+    var temp2 = ctx.fillStyle;
+
     ctx.lineWidth = 4;
 
     ctx.beginPath()
@@ -41,32 +60,39 @@ function drawEdge(x1, y1, x2, y2){
     ctx.stroke();
 
     drawCircle(x1, y1, true, 'red');
+    ctx.strokeStyle = 'white';
+    ctx.strokeText(String(verts.length-2),x1-4,y1+4);
+    ctx.strokeStyle = temp1;
     drawCircle(x2, y2, true, 'red');
+    ctx.strokeStyle = 'white';
+    ctx.strokeText(String(verts.length-1),x2-4,y2+4);
+
+    ctx.strokeStyle = temp1;
+    ctx.fillStyle = temp2;
 }
 
-for (var y = (circle_radius+circle_spacing)/2; y < wdow.height; y += circle_radius+circle_spacing) {
-    rows.push(y);
-    for (var x = (circle_radius+circle_spacing)/2; x < wdow.width-circle_spacing/2; x += circle_radius+circle_spacing) {
-        drawCircle(x, y)
-        cols.push(x);
-    }
+function considerVert(x, y){
+    //check if there is a vertex at this position. 
+    //if not, add this vertex and draw it.\
+    //only call on this method on second click.
 }
 
+var numClicks = 0;
 var cPos = [0, 0];
-verts = [];
-edges = [];
+var verts = new Map();
+var edges = new Map();
 
 wdow.addEventListener('mousedown', (event) => {
+    numClicks++;
     x = rows[Math.floor((event.pageX-wdow.offsetLeft+0.5)/(circle_radius+circle_spacing))];
     y = cols[Math.floor((event.pageY-wdow.offsetTop+0.5)/(circle_radius+circle_spacing))];
     drawCircle(x, y, true, 'grey', 'black');
-    cPos = [x, y];
-    verts.push(cPos);
-    if(verts.length%2==0){
-        var start = verts[verts.length-2];
-            console.log(start);
-        var end = verts[verts.length-1];
-            console.log(end);
-        drawEdge(start[0], start[1], end[0], end[1]);
-    }
+
+    // cPos = [x, y];
+    // verts.push(cPos);
+    // if(verts.length%2==0){
+    //     var start = verts[verts.length-2];
+    //     var end = verts[verts.length-1];
+    //     drawEdge(start[0], start[1], end[0], end[1]);
+    // }
 })
--- js/text.js
+++ js/text.js
@@ -1,13 +1,14 @@
 var instructions = document.getElementById('instructions');
 
-var intro = 'Click on the circles to create a node. Every second click creates a link.';
+var intro = 'Click to select a circle. Click on that circle a second time to create a Vertex, ' + 
+             'or click on a different circle to create a Link. ' + 
+             'If the circle you selected already contains a Vertex,  a new one will not be created but you may connect existing Verteces.';
 
 const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 async function printText(output, pace=50) {
   for (let i = 1; i <= output.length; i++) {
     await sleepNow(pace);
-    console.log("tick")
     instructions.innerText = output.substring(0, i);
   }
 }
