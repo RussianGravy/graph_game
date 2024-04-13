191684f5a87955f1435b76b388e60823121b707d
RussianGravy <valentinejones1215@gmail.com>
2024-04-13 00:49:07 -0700

    new folder. moved stuff

--- js/text.js
+++ js/text.js
@@ -1,12 +1,10 @@
 var instructions = document.getElementById('instructions');
 
-var pace = 50;
-
 var intro = 'Click on the circles to create a node. Every second click creates a link.';
 
 const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-async function printText(output) {
+async function printText(output, pace=50) {
   for (let i = 1; i <= output.length; i++) {
     await sleepNow(pace);
     console.log("tick")
