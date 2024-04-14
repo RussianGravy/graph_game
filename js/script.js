ebf0e0e5186afd4f839b84e60f9aba7ce2ebf6e6
RussianGravy <valentinejones1215@gmail.com>
2024-04-13 23:41:00 -0700

    added 2 layes for window

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
-const wdow = document.getElementById('window')
-const ctx = wdow.getContext("2d")
+const topLay = document.getElementById('layer_top')
+const ctxTop = topLay.getContext("2d")
+const botLay = document.getElementById('layer_bottom')
+const ctxBot = botLay.getContext("2d")
 
-wdow.width = wdow.offsetWidth; //500
-wdow.height = wdow.offsetHeight; //400
+topLay.width = topLay.offsetWidth; //500
+topLay.height = topLay.offsetHeight; //400
 
-circle_radius = wdow.width/60;
-circle_spacing = wdow.width/20;
+botLay.width = botLay.offsetWidth; //500
+botLay.height = botLay.offsetHeight; //400
+
+circle_radius = topLay.width/60;
+circle_spacing = topLay.width/20;
 
 var rows = [];
 var cols =[];
 
-for (var y = (circle_radius+circle_spacing)/2; y < wdow.height; y += circle_radius+circle_spacing) {
+for (var y = (circle_radius+circle_spacing)/2; y < topLay.height; y += circle_radius+circle_spacing) {
     rows.push(y);
-    for (var x = (circle_radius+circle_spacing)/2; x < wdow.width-circle_spacing/2; x += circle_radius+circle_spacing) {
+    for (var x = (circle_radius+circle_spacing)/2; x < topLay.width-circle_spacing/2; x += circle_radius+circle_spacing) {
         drawCircle(x, y)
         cols.push(x);
     }
 }
 
 function drawCircle(x, y, fill = false, fillColor = 'grey', stroke = 'grey'){
-    var temp1 = ctx.strokeStyle;
-    var temp2 = ctx.fillStyle;
-    var temp3 = ctx.lineWidth;
+    var temp1 = ctxTop.strokeStyle;
+    var temp2 = ctxTop.fillStyle;
+    var temp3 = ctxTop.lineWidth;
 
-    ctx.strokeStyle = stroke;
-    ctx.fillStyle = fillColor;
-    ctx.lineWidth = 1;
+    ctxTop.strokeStyle = stroke;
+    ctxTop.fillStyle = fillColor;
+    ctxTop.lineWidth = 1;
 
-    ctx.beginPath()
+    ctxTop.beginPath()
 
-    ctx.arc(x, y, circle_radius, 0, 2 * Math.PI);
+    ctxTop.arc(x, y, circle_radius, 0, 2 * Math.PI);
 
-    ctx.closePath()
+    ctxTop.closePath()
 
-    ctx.stroke();
+    ctxTop.stroke();
     
     if(fill){
-        ctx.fill();}
+        ctxTop.fill();}
 
-    ctx.strokeStyle = temp1;
-    ctx.fillStyle = temp2;
-    ctx.lineWidth = temp3;
+    ctxTop.strokeStyle = temp1;
+    ctxTop.fillStyle = temp2;
+    ctxTop.lineWidth = temp3;
 }
 
 function drawVert(){
@@ -48,12 +53,8 @@ function drawVert(){
 }
 
 function drawEdge(x1, y1, x2, y2){
-    var temp1 = ctx.strokeStyle;
-    var temp2 = ctx.fillStyle;
-    var temp3 = ctx.lineWidth;
-
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = 'black';
+    ctxBot.lineWidth = 4;
+    ctxBot.strokeStyle = 'black';
 
     var rat;
 
@@ -62,19 +63,15 @@ function drawEdge(x1, y1, x2, y2){
         Math.abs(0)
     ]; 
 
-    ctx.beginPath();
+    ctxBot.beginPath();
 
-    ctx.moveTo(x1, y1);
+    ctxBot.moveTo(x1, y1);
 
-    ctx.lineTo(x2, y2);
+    ctxBot.lineTo(x2, y2);
 
-    ctx.closePath();
+    ctxBot.closePath();
 
-    ctx.stroke();
-
-    ctx.strokeStyle = temp1;
-    ctx.fillStyle = temp2;
-    ctx.lineWidth = temp3;
+    ctxBot.stroke();
 }
 
 function makeKey(x, y){
@@ -86,8 +83,8 @@ function considerVert(x, y){
     if(!verts.has(key)){
         verts.set(key, verts.size+1);
         drawCircle(x, y, true, 'red');
-        ctx.strokeStyle = 'white';
-        ctx.strokeText(verts.size,x-4,y+4);
+        ctxTop.strokeStyle = 'white';
+        ctxTop.strokeText(verts.size,x-4,y+4);
         return true;
     }
     else 
@@ -100,10 +97,10 @@ var cPos = [0, 0];
 var verts = new Map();
 var edges = new Map();
 
-wdow.addEventListener('mousedown', (event) => {
+topLay.addEventListener('mousedown', (event) => {
     numClicks++;
-    x = rows[Math.floor((event.pageX-wdow.offsetLeft+0.5)/(circle_radius+circle_spacing))];
-    y = cols[Math.floor((event.pageY-wdow.offsetTop+0.5)/(circle_radius+circle_spacing))];
+    x = rows[Math.floor((event.pageX-topLay.offsetLeft+0.5)/(circle_radius+circle_spacing))];
+    y = cols[Math.floor((event.pageY-topLay.offsetTop+0.5)/(circle_radius+circle_spacing))];
     if(!verts.has( makeKey(x, y) )){
         drawCircle(x, y, true, 'grey', 'black');
     }
