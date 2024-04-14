218a4856482d118445802fc82eba5f7a0296998c
RussianGravy <valentinejones1215@gmail.com>
2024-04-13 23:22:54 -0700

    updated buttons

--- js/logic.js
+++ js/logic.js
@@ -11,9 +11,22 @@ function start(){
     printText(intro); 
 }
 
-function about(){
-    deleteButtons();
+async function about(){
+    var buttons = document.getElementsByTagName('button');
+    var temp = [];
+    for(var i = 0; i < buttons.length; i++){
+        buttons[i].style.opacity = 0;
+        buttons[i].style.opacity = 0;
+        buttons[i].style.position = 'absolute';
+    }
     printText(
         'Dijstra is a math based puzzle game. Solve the prompts and earn points!'
     );
+    await sleepNow(4000);
+    instructions.innerText = '';
+    for(var i = 0; i < buttons.length; i++){
+        buttons[i].style.opacity = 1;
+        buttons[i].style.opacity = 1;
+        buttons[i].style.position = 'static';
+    }
 }
